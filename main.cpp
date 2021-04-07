1b14b43b6d31fb0cf905274b1514f5f04bb448e7
whateverscript <81526814+whateverscript@users.noreply.github.com>
2021-04-07 08:24:04 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,36 +1,44 @@
-/* programa para imprimir hello world usando openMP na linguagem C*/
-#include <omp.h> //biblioteca openMP
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+#include <cstdlib>
+using namespace std;
 
-int main(int argc, char* argv[])
-{
+typedef struct aluno{
+string nome;
+float n1, n2, media;
+aluno *prox;
+aluno *proxN;
+int *ponteiroA;
+int *ponteiroN;
+} hmm;
 
-//imprimindo na região serial
-printf("\tOla! ---- estou fora da regiao paralela ----- \n\n");
+int x=0;
+int *i=&x;
 
-	//começo da região paralela (FORK)
-	#pragma omp parallel num_threads(7) //obs: também podemos usar omp_set_num_threads(7) para definir o numero de threads
-	{
-	    //pegando o ID de cada thread
-        int id= omp_get_thread_num();
+aluno *criar_no (){
+aluno *novo=(aluno*)malloc(sizeof(aluno));
+return novo;
+}
 
-        //pegando o número total de threads
-        int n_Thread= omp_get_num_threads();
+aluno *inserir_aluno(aluno *lista){
+aluno *novo=criar_no();
+novo->ponteiroA=i;
+string name;
+cout <<"nome: "<<*i<<endl;
+cin>> name;
+novo->nome=name;
+if (lista == NULL){
+    lista=novo;
+    novo->prox=NULL;
+}else{
+ aluno *aux=lista;
+ while (aux->prox != NULL){
+    aux=aux->prox;
+ }
+ novo->prox=NULL;
+ aux->prox=novo;
 
-		if (id == 0){ //processo
-		printf ("Hello World... sou a Master Thread (%d) dentro da regiao paralela\n", id);
-		} else {
-		printf("Hello World... sou a thread = %d de um total de %d \n", id, n_Thread);
-		}
-
-	} //FIM DA REGIÃO PARALELA (JOIN)
-
-//imprimindo na região serial
-printf("\n\tOla! ---- estou fora da regiao paralela ----- \n\n");
-
-
-return 0;
+}
+return lista;
 }
 
 
@@ -38,3 +46,140 @@ return 0;
 
 
 
+aluno *inserir_nota2(aluno *notas, float nt1, float nt2){
+aluno *nova=criar_no();
+nova->ponteiroN=i;
+nova->n1=nt1;
+nova->n2=nt2;
+nova->media= ((nova->n1 + nova->n2)/2);
+if (notas == NULL){
+    notas=nova;
+    nova->prox=NULL;
+    }else{
+       aluno *aux=notas;
+       while (aux->prox !=NULL ) {
+        aux=aux->prox;
+       }
+       nova->prox=NULL;
+       aux->prox=nova;
+    }
+
+    return notas;
+}
+
+
+
+
+
+aluno* inserir_nota (aluno *notas, aluno *lista){
+aluno *aux1=lista;
+aluno *aux=notas;
+int num;
+float n1, n2;
+cout <<"digite o numero do aluno "<<endl;
+cin >>num;
+while (aux1->ponteiroA != i){
+    aux1=aux1->prox;
+}
+if (aux1->ponteiroA == i){
+            cout <<endl<<endl<<"aluno encontrado!"<<endl;
+            cout <<endl<<endl<<"digite as notas: "<<endl;
+            cout <<"Nota 1: ";
+            cin >>n1;
+            cout <<endl<<"Nota 2: ";
+            cin >>n2;
+            notas=inserir_nota2(notas, n1, n2);
+
+            aux1->prox=notas;
+       }
+return notas;
+
+}
+
+
+aluno * media(aluno *lista, aluno *notas){
+aluno *aux=notas;
+aluno *aux1=lista;
+string nome;
+cout <<"digite o nome do aluno: "<<endl;
+cin>> nome;
+
+while (aux1!= NULL && aux1->nome != nome){
+    aux1=aux1->prox;
+    aux= aux->proxN;
+}
+if (aux1->nome == nome && aux == NULL){
+    cout <<"aluno sem nota"<<endl;
+    return  NULL;
+}
+if (aux1->nome == nome && aux != NULL){
+cout <<"\tAluno encontrado!"<<endl;
+cout <<" MEDIA: "<<aux->media<<" ";
+
+if (aux->media >=7){
+    cout <<"Aprovado!"<<endl;
+}else {
+if (aux->media <7){
+    cout <<"Reprovado!"<<endl;
+}
+
+}return notas;}
+
+}
+
+
+
+
+void imprimir (aluno *lista, aluno* notas){
+aluno *aux=lista;
+aluno *aux1=notas;
+while (aux!= NULL && aux1!=NULL){
+
+    cout <<"aluno :"<<aux->nome<<endl;
+    cout <<"media: "<<aux1->media<<endl;
+    aux=aux->prox;
+    aux1=aux1->proxN;
+}
+
+}
+
+
+
+
+int main()
+{
+    short op;
+    string nome;
+    aluno *lista=NULL;
+    aluno *notas = NULL;
+    do {
+            x=x+2;
+        cout <<"1 inserir aluno"<<endl<<"2 iserir nota"<<endl<< "3 MEDIA" <<endl<<"4 IMPRIMIR"<<endl;
+        cin >>op;
+        switch (op){
+    case 1:
+        --x;
+        lista=inserir_aluno(lista);
+        cout <<"valor de x agora: "<<x<<endl;
+        break;
+
+    case 2:
+        x=x-2;
+        notas=inserir_nota(notas, lista);
+        cout <<"valor de x agora: "<<x<<endl;
+        break;
+    case 3:
+        x=x-2;
+        media(lista, notas);
+        cout <<"valor de x agora: "<<x<<endl;
+        break;
+    case 4:
+        imprimir(lista, notas);
+        break;
+        }
+
+    }while (op!=0);
+    cin.get();
+    cin.get();
+    return 0;
+}
